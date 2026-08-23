0caba4c14d28a91bb3809ff33e680b240b6cb481
Prateekkhatrii <prateekkhatri028@gmail.com>
2026-08-23 18:21:00 +0530

    Implement Single Number problem
    Add initial implementation for Single Number problem.

---
+++ Bit manipulation/136_single_number.py
@@ -0,0 +1,15 @@
+#136. Single Number
+'''Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.'''
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result=0
+        for i in nums:
+            result=result^i
+        return result
+                
