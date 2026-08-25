9a090d49b22fb8091038e24c7626c4c88172acd6
Prateekkhatrii <prateekkhatri028@gmail.com>
2026-08-25 11:16:10 +0530

    Add function to compute divisors of a number
    Implement a function to find all divisors of a number in sorted order.

---
+++ Bit manipulation/print_divisors.py
@@ -0,0 +1,25 @@
+'''
+Divisors of a Number
+You are given an integer n. You need to find all the divisors of n. Return all the divisors of n as an array or list in a sorted order.
+A number which completely divides another number is called it's divisor.
+
+Example 1
+Input: n = 6
+Output = [1, 2, 3, 6]
+Explanation: The divisors of 6 are 1, 2, 3, 6.
+
+Example 2
+Input: n = 8
+Output: [1, 2, 4, 8]
+Explanation: The divisors of 8 are 1, 2, 4, 8.
+'''
+import math
+class Solution:
+    def divisors(self, n):
+        ans=[]
+        for i in range(1,int(math.sqrt(n))+1):
+            if n%i==0:
+                ans.append(i)
+                if n//i!=i:
+                    ans.append(n//i) 
+        return sorted(ans)       
