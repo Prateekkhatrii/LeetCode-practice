a251a31099433863d796d9270a67e2cfc9a3f880
Prateekkhatrii <prateekkhatri028@gmail.com>
2026-08-27 13:09:29 +0530

    Implement stack using a queue

---
+++ stacks_and_queues/stack_using_queue.py
@@ -0,0 +1,48 @@
+class MyStack(object):
+
+    def __init__(self):
+        self.queue=[]
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.queue.append(x)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        for i in range(len(self.queue) - 1):
+            x = self.queue.pop(0)
+            self.queue.append(x)
+
+        return self.queue.pop(0)
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        for i in range(len(self.queue) - 1):
+            x = self.queue.pop(0)
+            self.queue.append(x)
+
+        x = self.queue.pop(0)
+        self.queue.append(x)
+
+        return x
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return len(self.queue) == 0
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
